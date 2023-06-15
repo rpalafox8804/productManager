a17150d5d02b294fc08f8b7e3089540781d2b90b
Ricardo Palafox <rpalafox8804@gmail.com>
2023-06-15 09:31:50 -0700

    Part 3

--- client/src/components/ViewProduct.js
+++ client/src/components/ViewProduct.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 
 
 const ViewProduct = () => {
   const {id} = useParams()
   const [state, setState] = useState({})
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`http://localhost:8000/api/products/${id}`)  
@@ -17,6 +20,18 @@ const ViewProduct = () => {
         console.log(err)
       })
   }, [])
+
+  const deleteHandler = (id) => {
+
+    axios.delete(`http://localhost:8000/api/products/${id}`)
+      .then((res)=>{
+        console.log(res)
+        navigate("/products")
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  }
   return (
     <div>
       <h1>Product Information</h1>
@@ -24,6 +39,7 @@ const ViewProduct = () => {
       <h3>Price: {state.price}</h3>
       <h3>Description: {state.description}</h3>
       <Link to={`/products`} className='btn btn-sm btn-warning'>Dashboard</Link>
+      <button className='btn btn-sm btn-danger' onClick={() => deleteHandler (state._id)}> Delete</button>
     </div>
   )
 }
