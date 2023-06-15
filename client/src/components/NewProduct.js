be436a5ff9445a50421f15de2c92cc3c982f9993
Ricardo Palafox <rpalafox8804@gmail.com>
2023-06-15 09:20:36 -0700

    Part 2

--- client/src/components/NewProduct.js
+++ client/src/components/NewProduct.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Dashboard from './Dashboard'
+
+
 
 const NewProduct = () => {
   const [state, setState] = useState({name:"", price:0, description:""});
@@ -17,22 +21,46 @@ const NewProduct = () => {
     const submitHandler = (e) => {
       e.preventDefault();
       axios.post("http://localhost:8000/api/products", state)
-        .then((res)=>{
+      .then((res)=>{
+
           console.log(res.data)
+          setProductState([...productState, res.data]); 
           navigate("/products")
         })
         .catch((err)=>{
-          const errors =(err.response.data.errors.errors)
-
-          const errorArr = []
-          for (const key of Object.keys(errors)){
-            errorArr.push(errors[key].message)
-          }
-          setErrors(errorArr)
-
+          // const errors =(err.response.data.errors.errors)
+          // const errorArr = []
+          // for (const key of Object.keys(errors)){
+          //   errorArr.push(errors[key].message)
+          // }
+          // setErrors(errorArr)
+          console.log(err)
         })
+
     }
-  
+  const [productState, setProductState] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/products")
+      .then((res)=>{
+        setProductState(res.data)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  }, [])
+  const deleteHandler = (id) => {
+    const newList = productState.filter((product, idx) => (product._id != id) )
+    setProductState(newList)
+
+    axios.delete(`http://localhost:8000/api/products/${id}`)
+      .then((res)=>{
+        console.log(res)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    }
+
   return (
     <div>
       {/* {errors && errors.map((err, i)=>(
@@ -57,6 +85,38 @@ const NewProduct = () => {
           <input className='btn btn-primary btn-sm' type="submit" value="Create Product"/>
         </div>
       </form>
+       <div>
+      <h1>Dashboard</h1>
+      <table className='table table-striped table-bordered'>
+        <thead className='bg-primary'>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            productState.map((product, i)=>{
+
+              return (
+                <tr key={i}>
+                  <td><Link to={`/products/${product._id}`}>{product.title}</Link></td>
+                  <td>{product.price}</td>
+                  <td>{product.description}</td>
+                  <td>
+                    <Link to={`/products/${product._id}/edit`} className='btn btn-sm btn-warning'>Edit</Link>
+                    <button className='btn btn-sm btn-danger' onClick={() => deleteHandler (product._id)}> Delete</button>
+                  </td>
+                </tr>
+              )
+            }
+            )
+          }
+        </tbody>
+      </table>
+      </div>
     </div>
   )
 }
